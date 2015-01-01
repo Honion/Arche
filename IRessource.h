0392a4b79ce5c73307f6bdb0a968f8bbc42b34c9
Voisin-Alexis <bernabe.voisin@laposte.net>
2015-01-01 19:14:00 +0100

    Update IRessource.h

--- IRessource.h
+++ IRessource.h
@@ -1,5 +1,23 @@
-#include "Devoir.h"
-#include "Url.h"
-#include "Fichier.h"
+#ifndef __IRessource_H__
+#define __IRessource_H__
+
+#include<string>
+
+class Devoir;
+class Url;
+class Fichier;
 
 
+class IRessource{
+      public:
+      	IRessource();
+    	 std::string getTitre();
+    	void setTitre(std::string titre);
+       
+        
+        protected:
+        	std::string Titre;
+          
+             
+};
+#endif
