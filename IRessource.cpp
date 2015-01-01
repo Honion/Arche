272770cdb6ea9f9768091788e269c547003c86bd
Voisin-Alexis <bernabe.voisin@laposte.net>
2015-01-01 22:31:57 +0100

    Update IRessource.cpp

---
+++ IRessource.cpp
@@ -0,0 +1,20 @@
+#include<string>
+#include<list>
+#include<iostream>
+#include <vector>
+#include"Devoir.h"
+#include"Url.h"
+#include"Fichier.h"
+
+
+using namespace std;
+std::string Titre;
+          
+          std::string getTitre(){
+          	return Titre;
+		  }
+		  
+    	void setTitre(std::string titre){
+    		Titre = titre;
+		}
+       
