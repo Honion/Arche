628f8834a3118ce16c399d3dc521867079c32ac8
Voisin-Alexis <bernabe.voisin@laposte.net>
2015-01-01 21:17:34 +0100

    Update Administrateur.cpp

--- Administrateur.cpp
+++ Administrateur.cpp
@@ -1,1 +1,30 @@
-#include "administrateur.h"
+#include<string>
+#include<list>
+#include<iostream>
+#include"Administrateur.h"
+
+
+using namespace std;
+std::string Identifiant;
+std::string Mdp;
+
+
+            std::string getIdentifiant(){
+            	return Identifiant;
+			}
+			
+             void setIdentifiant(std::string id){
+             	Identifiant = id;
+			 }
+			 
+             std::string getMdp(){
+             	return Mdp;
+			 }
+			 
+             void setMdp(std::string mdp){
+             	Mdp = mdp;
+			 }
+			 
+             void ajouterCours(){
+             	
+			 }
