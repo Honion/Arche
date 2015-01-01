72bec2a6c1e7caa2f3e1755496f88f1c558bc799
Voisin-Alexis <bernabe.voisin@laposte.net>
2015-01-01 22:36:09 +0100

    Update IPersonne.cpp

--- IPersonne.cpp
+++ IPersonne.cpp
@@ -1,5 +1,30 @@
+#include<string>
+#include<list>
+#include<iostream>
+#include<vector>
+
 #include "Eleve.h"
 #include "Enseignant.h"
 #include "Administrateur.h"
 #include "Cours.h"
 #include "IPersonne.h"
+
+using namespace std;
+	std::string Identifiant;
+	std::string Mdp;
+	
+		 std::string getIdentifiant(){
+		 	return Identifiant;
+		 }
+		 
+    	void setIdentifiant(std::string id){
+    		Identifiant = id;
+		}
+		
+        std::string getMdp(){
+        	return Mdp;
+		}
+		
+        void setMdp(std::string mdp){
+        	Mdp = mdp;
+		}
