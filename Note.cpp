cce28287c742781f936dc4f776434e8ec74e63f6
Voisin-Alexis <bernabe.voisin@laposte.net>
2015-01-01 22:40:41 +0100

    Update Note.cpp

--- Note.cpp
+++ Note.cpp
@@ -1,1 +1,18 @@
-	
+#include<string>
+#include<list>
+#include<iostream>
+#include"Note.h"
+
+
+using namespace std;
+int note;
+
+
+  		void setNote(int note){
+  			note = note;
+  		}
+  				
+             	int getNote(){
+             		return note;
+				 }
+
